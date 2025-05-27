3612fa95d887452ebc165e0d38d8fc8a6a31e19d
Ankur Pandey <zsankur8289@gmail.com>
2025-05-27 11:21:02 +0100

    Adding code and test files

---
+++ hello1.py
@@ -0,0 +1,15 @@
+from hello import add, sub
+
+def setup():
+    print("Setting up the tests")
+
+def teardown():
+    print("tearing down the tests")
+
+def test_add():
+    assert add(3, 5) == 8, "Test case 1 failed"
+    assert add(3, 5) == 9, "Test case 2 failed"
+
+print(__name__)
+if __name__ == "__main__":
+    test_add()
